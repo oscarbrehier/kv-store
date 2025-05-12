27199e1fba627b9ff3ad9f9ed794d1bf4da17a59
Oscar Bréhier <68658005+oscarbrehier@users.noreply.github.com>
2025-05-12 12:15:40 +0200

    added log level

--- includes/status_codes.h
+++ includes/status_codes.h
@@ -2,38 +2,39 @@
 # define STATUS_CODES_H
 
 #define STATUS_LIST \
-    X(SUCCESS, 0, "OK.") \
-    X(WARNING_KEY_EXISTS, 1, "Warning: Key already exists. Value will be updated.") \
-    X(ERROR_MEMORY_ALLOCATION, 2, "Error: Failed to allocate memory for key-value pair.") \
-    X(ERROR_MEMORY_REALLOCATION, 3, "Error: Failed to Reallocate memory.") \
-    X(ERROR_INVALID_INPUT, 4, "Error: Invalid key or value format.") \
-    X(ERROR_KEY_NOT_FOUND, 5, "Error: Key not found.") \
-    X(ERROR_FILE_OPEN, 6, "Error: Unable to open file for saving data.") \
-    X(ERROR_FILE_WRITE, 7, "Error: Failed to write data to file.") \
-    X(ERROR_FILE_READ, 8, "Error: Unable to open file for loading data.") \
-    X(ERROR_FILE_FORMAT, 9, "Error: Invalid file format. Unable to parse key-value pairs.") \
-    X(ERROR_FILE_CLOSE, 10, "Error: Failed to close file.") \
-    X(ERROR_INVALID_COMMAND, 11, "Error: Invalid command. Type 'help' for a list of available commands.") \
-    X(ERROR_FILE_SAVE, 12, "Error: Failed to save data to file before exit.") \
-    X(ERROR_UNEXPECTED_EXIT, 13, "Error: An unexpected error occurred during exit. Data may not have been saved.") \
-    X(ERROR_HELP_SYNTAX, 14, "Error: Incorrect usage of the command. Use 'help' to see the correct syntax.") \
-    X(ERROR_FILE_HEADER, 15, "Error: Invalid file header.") \
-    X(ERROR_READ_KEY, 16, "Error : Failed to read key from file.") \
-    X(ERROR_READ_VAL, 17, "Error: Failed to read value from file.") \
-    X(ERROR_READ_VAL_LEN, 18, "Error: Failed to read value length from file.") \
-    X(ERROR_INIT_TABLE, 19, "Error: Failed to initialize table.") \
-    X(ERROR_TABLE_MISSING, 20, "Error: The specified table could not be found.") \
-    X(ERROR_TABLE_DROP, 21, "Error: Failed to drop the specified table.") \
-    X(ERROR_TABLE_RENAME, 22, "Error: Failed to rename the specified table.") \
-    X(ERROR_UNEXPECTED, 22, "Error: An unexpected error occurred.") \
-    X(ERROR_COMMAND_TABLE_MISSING, 23, "Error: Command table has not been initalized.") \
-    X(ERROR_COMMAND_STRUCT_NOT_FOUND, 24, "Error: Command not found.") \
-    X(ERROR_COMMAND_NAME_MISSING, 25, "Error: Command name is missing.") \
-    X(ERROR_VALUE_TYPE_MISMATCH, 26, "Error: Value type mismatch.") \
-    X(ERROR_STR_TO_INT, 27, "Error: Failed to convert string to integer.") \
-    X(ERROR_MUTEX_INIT, 27, "Error: Failed to table mutex.") \
+	X(UNKNOWN, -1, "unknown error", 4) \
+    X(SUCCESS, 0, "OK.", 2) \
+    X(WARNING_KEY_EXISTS, 1, "Warning: Key already exists. Value will be updated.", 3) \
+    X(ERROR_MEMORY_ALLOCATION, 2, "Error: Failed to allocate memory for key-value pair.", 4) \
+    X(ERROR_MEMORY_REALLOCATION, 3, "Error: Failed to Reallocate memory.", 4) \
+    X(ERROR_INVALID_INPUT, 4, "Error: Invalid key or value format.", 4) \
+    X(ERROR_KEY_NOT_FOUND, 5, "Error: Key not found.", 4) \
+    X(ERROR_FILE_OPEN, 6, "Error: Unable to open file for saving data.", 4) \
+    X(ERROR_FILE_WRITE, 7, "Error: Failed to write data to file.", 4) \
+    X(ERROR_FILE_READ, 8, "Error: Unable to open file for loading data.", 4) \
+    X(ERROR_FILE_FORMAT, 9, "Error: Invalid file format. Unable to parse key-value pairs.", 4) \
+    X(ERROR_FILE_CLOSE, 10, "Error: Failed to close file.", 4) \
+    X(ERROR_INVALID_COMMAND, 11, "Error: Invalid command. Type 'help' for a list of available commands.", 4) \
+    X(ERROR_FILE_SAVE, 12, "Error: Failed to save data to file before exit.", 4) \
+    X(ERROR_UNEXPECTED_EXIT, 13, "Error: An unexpected error occurred during exit. Data may not have been saved.", 5) \
+    X(ERROR_HELP_SYNTAX, 14, "Error: Incorrect usage of the command. Use 'help' to see the correct syntax.", 4) \
+    X(ERROR_FILE_HEADER, 15, "Error: Invalid file header.", 4) \
+    X(ERROR_READ_KEY, 16, "Error: Failed to read key from file.", 4) \
+    X(ERROR_READ_VAL, 17, "Error: Failed to read value from file.", 4) \
+    X(ERROR_READ_VAL_LEN, 18, "Error: Failed to read value length from file.", 4) \
+    X(ERROR_INIT_TABLE, 19, "Error: Failed to initialize table.", 4) \
+    X(ERROR_TABLE_MISSING, 20, "Error: The specified table could not be found.", 4) \
+    X(ERROR_TABLE_DROP, 21, "Error: Failed to drop the specified table.", 4) \
+    X(ERROR_TABLE_RENAME, 22, "Error: Failed to rename the specified table.", 4) \
+    X(ERROR_UNEXPECTED, 22, "Error: An unexpected error occurred.", 5) \
+    X(ERROR_COMMAND_TABLE_MISSING, 23, "Error: Command table has not been initialized.", 4) \
+    X(ERROR_COMMAND_STRUCT_NOT_FOUND, 24, "Error: Command not found.", 4) \
+    X(ERROR_COMMAND_NAME_MISSING, 25, "Error: Command name is missing.", 4) \
+    X(ERROR_VALUE_TYPE_MISMATCH, 26, "Error: Value type mismatch.", 4) \
+    X(ERROR_STR_TO_INT, 27, "Error: Failed to convert string to integer.", 4) \
+    X(ERROR_MUTEX_INIT, 27, "Error: Failed to table mutex.", 4)
     
-#define X(name, code, message) name##_CODE = code,
+#define X(name, code, message, log_level) name##_CODE = code,
 
 typedef enum {
     STATUS_LIST
@@ -41,7 +42,14 @@ typedef enum {
 
 #undef X
 
-const char  *get_status(t_status_code code);
+typedef struct s_status
+{
+    t_status_code	code;
+    const char		*message;
+	int				log_level;
+} t_status;
+
+t_status	get_status(t_status_code code);
 void        log_message(int fd, t_status_code code, ...);
 
 #endif
--- srcs/libs/status_codes.c
+++ srcs/libs/status_codes.c
@@ -2,38 +2,46 @@
 #include "status_codes.h"
 #include "libs.h"
 
-#define X(name, code, message) {name##_CODE, message},
-
-const char  *get_status(t_status_code code)
+t_status	get_status(t_status_code code)
 {
-    size_t i;
+    size_t		i;
+	t_status	status = {UNKNOWN_CODE, "unkown error", 4};
     struct {
-        t_status_code code;
-        const char *message;
+        t_status_code	code;
+        const char		*message;
+		int				log_level;
     } status_table[] = {
+        #define X(name, code, message, log_level) {code, message, log_level},
         STATUS_LIST
+        #undef X
     };
 
     i = 0;
     while (i < sizeof(status_table) / sizeof(status_table[0]))
     {
         if (status_table[i].code == code)
-            return status_table[i].message;
+		{
+			status.code = status_table[i].code;
+			status.message = status_table[i].message;
+			status.log_level = status_table[i].log_level;
+			return (status);
+		}
         i++;
     }
-    return ("Error: Unexpected error.");
+	return (status);
 }
 
 #undef X
 
 void    log_message(int fd, t_status_code code, ...)
 {
-    const char *content = get_status(code);
+    t_status status;
     va_list args;
 
     if (code == SUCCESS_CODE)
         fd = 2;
     va_start(args, code);
-    logger(fd, (char *)content, args);
+	status = get_status(code);
+    logger(fd, (char *)status.message, args);
     va_end(args);
 }
